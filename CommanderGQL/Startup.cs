a24dcaa91ec12d16b1e7b68e0a211c6cff27b034
stevenliu88 <30949641+stevenliu88@users.noreply.github.com>
2021-11-19 13:10:38 +1100

    Connect to Db by using efc and create data models

--- CommanderGQL/Startup.cs
+++ CommanderGQL/Startup.cs
@@ -7,15 +7,25 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using CommanderGQL.Data;
 
 namespace CommanderGQL
 {
     public class Startup
     {
+        private readonly IConfiguration Configuration;
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer 
+            (Configuration.GetConnectionString("CommandConStr")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
